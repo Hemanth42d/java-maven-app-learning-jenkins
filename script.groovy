1ac37b6e8bc2e51fef82a37f7bf39cd4abf9e59e
Hemanth42d <venkatahemanth42d@gmail.com>
2025-07-31 22:55:43 +0530

    Updated script.groovy file

--- script.groovy
+++ script.groovy
@@ -1,15 +1,16 @@
 def buildApp() {
     withCredentials([
-        usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USER', passwordVariable: 'PWD')
+        usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')
     ]) {
         echo "Building the application...."
         sh "mvn clean package"
         sh "docker build -t hemanth42/java-maven-app:jma-1.0 ."
-        sh "echo \$PWD | docker login -u \$USER --password-stdin"
+        sh "echo \$PASS | docker login -u \$USER --password-stdin"
         sh "docker push hemanth42/java-maven-app:jma-1.0"
         echo "Docker image pushed successfully!"
     }
 }
+
 
 def testApp() {
     echo "Testing the application...."
