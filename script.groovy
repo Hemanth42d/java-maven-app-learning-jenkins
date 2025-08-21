5ee9fafa25d5ccd8a6a6adef50630682d68777bc
M.Venkata Hemanth <86419987+Hemanth42d@users.noreply.github.com>
2025-08-21 14:59:44 +0530

    Updated script.groovy

--- script.groovy
+++ script.groovy
@@ -4,9 +4,9 @@ def buildApp() {
     ]) {
         echo "Building the application...."
         sh "mvn clean package"
-        sh "docker build -t hemanth42/java-maven-app:jma-2.0 ."
+        sh "docker build -t hemanth42/java-maven-app:jma-2.1 ."
         sh "echo \$PASS | docker login -u \$USER --password-stdin"
-        sh "docker push hemanth42/java-maven-app:jma-2.0"
+        sh "docker push hemanth42/java-maven-app:jma-2.1"
         echo "Docker image pushed successfully!"
     }
 }
